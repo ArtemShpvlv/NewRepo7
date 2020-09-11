f5cdcfb3a6ee82b83ed1e3ace955aa5514d4f4f1
artem shapovalov <mishoooona@mail.ru>
2020-09-11 11:28:13 +0300

    123

--- pr1/01/ConsoleApp7/Program.cs
+++ pr1/01/ConsoleApp7/Program.cs
@@ -7,27 +7,24 @@ namespace PR1_1
     public string firstName;
     public string lastName;
     public int age;
-
-    public Person(string _firstName, string _lastName, int _age)
+    public Genders gender;
+    public enum Genders : int { Male, Female };
+    public Person(string _firstName, string _lastName, int _age , Genders _gender)
     {
       firstName = _firstName;
-      lastName = _lastName; age = _age;
+      lastName = _lastName; age = _age , gender = _gender;
     }
     public override string ToString()
     {
-      return firstName + " " + lastName + ", age " + age;
+      return firstName + " " + lastName + "("+ gender +"), age " + age;
     }
-
-
   }
-
   class Program
   {
     static void Main(string[] args)
     {
-      Person p = new Person("Rusakov", "Alexey", 35);
+      Person p = new Person("Rusakov", "Alexey", 35 , Person.Genders.Male);
       Console.WriteLine(p.ToString());
-      Console.ReadKey();
     }
   }
 }
