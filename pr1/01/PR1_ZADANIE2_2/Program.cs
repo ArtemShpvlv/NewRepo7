5c4528b3a353f4bf8227e2c377ecb57b64c0fdd6
artem shapovalov <mishoooona@mail.ru>
2020-09-11 12:00:48 +0300

    123

--- pr1/01/PR1_ZADANIE2_2/Program.cs
+++ pr1/01/PR1_ZADANIE2_2/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace PR1_ZADANIE2_1
+namespace _1
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+
+      SByte a = 0;
+      Byte b = 0;
+      Int16 c = 0;
+      Int32 d = 0;
+      Int64 e = 0;
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); Console.WriteLine(s);
+      Exception ex = new Exception();
+
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+
+      }
     }
   }
 }
+
+
